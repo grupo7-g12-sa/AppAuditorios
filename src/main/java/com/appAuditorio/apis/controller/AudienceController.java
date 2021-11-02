929e7a92c2c096d6b13207d216a56f60692b82a1
Reideh17 <hedier.alvarez@gmail.com>
2021-11-01 22:25:55 -0500

    reto 4

--- src/main/java/com/appAuditorio/apis/controller/AudienceController.java
+++ src/main/java/com/appAuditorio/apis/controller/AudienceController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -69,9 +70,16 @@ public class AudienceController {
      * @param auditorio objeto Json que se enviará.
      * @return código de estado de respuesta HTTP (204)
      */
-    @DeleteMapping("/delete")
+    /*@DeleteMapping("/delete")
     public ResponseEntity deleteAuditorio(@RequestBody Audience auditorio){
         servicioAud.deleteAuditorio(auditorio.getId());
+        
+        return ResponseEntity.status(204).build();
+    }*/
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable("id") int id){
+        servicioAud.deleteAuditorio(id);
         
         return ResponseEntity.status(204).build();
     }
